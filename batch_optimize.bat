68e25026f56ee6238dc8d1cf3c5c0262c0e1ce1b
Sin! <stkdm@yandex.ru>
2020-09-25 02:19:22 +1000

    Улучшение батника оптимизации привязки для работы с любыми директориями

--- batch_optimize.bat
+++ batch_optimize.bat
@@ -1,6 +1,7 @@
 echo off
-echo Start optimizing in directory %1 > LinksOptimizer.log
+echo Start optimizing in directory %1 > %~dp0\LinksOptimizer.log
 for /r "%1"  %%i in (*.ogf) do (
-echo ------------------  >> LinksOptimizer.log
-LinksOptimizer.exe "%%i" >> LinksOptimizer.log
+echo ------------------  >> %~dp0\LinksOptimizer.log
+echo Optimizing file %%i >> %~dp0\LinksOptimizer.log
+%~dp0\LinksOptimizer.exe "%%i" >> %~dp0\LinksOptimizer.log
 )
